b98e2cf8c8b31ef8d716830611315f322c9c31f3
Safoun <safoun.official@gmail.com>
2023-08-07 19:26:58 +0600

    solving tailwind issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div>Hello world!</div>,
+		element: (
+			<div className="text-3xl font-bold underline">Hello world!!!!!!!!!!!!</div>
+		),
 	},
 ]);
 
