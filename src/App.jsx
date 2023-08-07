7d4e3402938364cab795d6805d216912f970b2e6
jakariachynabil10 <jakariachynabil@gmail.com>
2023-08-07 21:32:27 +0600

    tailwind issu slove

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1 className='text-pink-500'>Hello world</h1>
+            <h1 className='text-pink-500'>
+      Hello world!
+    </h1>
         </div>
     );
 };
