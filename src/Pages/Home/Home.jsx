c4955e10843e574594ca6918ac86c563dbb8598a
jakariachynabil10 <jakariachynabil@gmail.com>
2023-08-10 05:16:52 +0600

    image is added on public and swiper is install also banner image is done but little more work is pending

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import { Helmet } from "react-helmet-async";
 import FlipCard from "./Components/Fetured/FlipCard";
 import "./Home.css";
 import SiteStates from "./Components/SiteStates/SiteStates";
+import Banner from "./Components/Banner";
 const Home = () => {
   return (
     <div className="bg-[url('https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg')]">
       <Helmet>
         <title>TitanArena | Home</title>
       </Helmet>
+      <Banner/>
       <FlipCard />
       <AllGames />
       <FeatureGames />
