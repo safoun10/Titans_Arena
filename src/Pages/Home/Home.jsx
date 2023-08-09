83edd8204cc041f095c53a8b5decbd8e5b15bceb
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-08-09 22:17:32 +0600

    games card updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import FeatureGames from "./Components/FeatureGames/FeatureGames";
+import Games from "./Components/AllGames/AllGames";
 import "./Home.css";
 const Home = () => {
   return (
-    <div>
-      <h2 className="">TitanArena</h2>
-      <p>Bangladesh</p>
+    <div className="bg-[url('https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg')]">
+      <Games />
+      <FeatureGames />
     </div>
   );
 };
