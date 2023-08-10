97ef18cd08f5e5c1df7f0560ea2bf44c8788b09e
Saiful Islam <91333249+Saiful264@users.noreply.github.com>
2023-08-11 00:02:25 +0600

    Update Root.jsx

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../Components/Shared/NavigationBar";
-import Navbar from "../Components/Shared/Navbar";
 import Footer from "../Components/Shared/Footer";
 
 const Root = () => {
   return (
     <div>
       <NavigationBar/>
-      {/* <Navbar/> */}
       <Outlet />
       <Footer/>
     </div>
