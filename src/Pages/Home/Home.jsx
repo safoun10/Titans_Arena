4a4768994c3d5184328c55cfcd398bc4f873d572
numanrahi <arahi2994@gmail.com>
2023-08-10 23:28:23 +0600

    review section basic design added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import FlipCard from "./Components/Fetured/FlipCard";
+import ReviewSection from "./Components/ReviewSection/ReviewSection";
 import "./Home.css";
 const Home = () => {
   return (
     <div>
       <h2 className="">TitanArena</h2>
       <FlipCard></FlipCard>
+      <ReviewSection></ReviewSection>
     </div>
   );
 };
