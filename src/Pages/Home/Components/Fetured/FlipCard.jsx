c49f09964ccd0b52450c2b1a4c62043fe54ef9c1
jakariachynabil10 <jakariachynabil@gmail.com>
2023-08-24 19:32:29 +0600

    flipcard section is now dynamic

--- src/Pages/Home/Components/Fetured/FlipCard.jsx
+++ src/Pages/Home/Components/Fetured/FlipCard.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FlipCard.css";
 import Title from "../../../../Components/Shared/AnimatedTitle/Titile";
+import { Link } from "react-router-dom";
 
 const FlipCard = () => {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    fetch("https://titans-arena-server.vercel.app/games")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setGames(data.slice(0, 4));
+      });
+  }, []);
   const cardData = [
     {
       image: "https://wallpapercave.com/wp/wp7897048.jpg",
@@ -33,16 +43,18 @@ const FlipCard = () => {
       <div className="container mx-auto">
         <div className="max-w-6xl mx-auto">
           <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 ">
-            {cardData.map((card, index) => (
+            {games.map((card, index) => (
               <div key={index} className="book">
                 <div className="page">
-                  <div
-                    className="front-page  bg-cover bg-center relative border border-blue-500"
-                    style={{ backgroundImage: `url(${card.image})` }}
-                  >
+                  <div >
+                    <img
+                      src={card.image}
+                      className="front-page  bg-cover bg-center relative border border-blue-500"
+                      alt=""
+                    />
                     <div className="after-content bg-center absolute inset-0 opacity-0 hover:opacity-100 transition duration-300">
                       <img
-                        src={card.contentImage}
+                        src={card.image}
                         alt="Content"
                         className="h-full w-full"
                       />
@@ -50,7 +62,9 @@ const FlipCard = () => {
                   </div>
                   <div className="back-page">
                     <p>Surprise! SAFOAN BROOOOOOOOOOO</p>
-                    <button className="btn">{card.buttonText}</button>
+                    <Link to={`/games/${card._id}`}>
+                    <button className="btn">Details</button>
+                    </Link>
                   </div>
                 </div>
               </div>
