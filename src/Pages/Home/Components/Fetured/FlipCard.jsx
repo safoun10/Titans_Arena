d0afef87dec8ad238757fbb425e4b344203d612b
jakariachynabil10 <jakariachynabil@gmail.com>
2023-08-25 18:47:06 +0600

    flip card is done and responsive also done

--- src/Pages/Home/Components/Fetured/FlipCard.jsx
+++ src/Pages/Home/Components/Fetured/FlipCard.jsx
@@ -9,66 +9,49 @@ const FlipCard = () => {
     fetch("https://titans-arena-server.vercel.app/games")
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data)
         setGames(data.slice(0, 4));
       });
   }, []);
+
   const cardData = [
-    {
-      image: "https://wallpapercave.com/wp/wp7897048.jpg",
-      buttonText: " Details",
-    },
-    {
-      image:
-        "https://www.roccat.com/cdn/shop/articles/unnamed_85098cea-a4de-47ed-b6f7-90d1fa64f116.jpg?v=1623429494",
-      buttonText: "Details",
-    },
-    {
-      image: "https://images.hdqwalls.com/wallpapers/battlefield-game-4k.jpg",
-      buttonText: "View Details",
-    },
-    {
-      image:
-        "https://i.ytimg.com/vi/tj6FGxy5WuI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCJ0NkD5Chs5TG-Dg-qgTcMnWA4Pw",
-      buttonText: "Details",
-    },
+    // Card data objects
   ];
+
   return (
     <div>
-      <Title
-        primaryText="WELCOME OUR GALLERY SECTION"
-        secondaryText="it's awesome!"
-      />
+      <Title primaryText="WELCOME OUR GALLERY SECTION" secondaryText="it's awesome!" />
 
       <div className="container mx-auto">
         <div className="max-w-6xl mx-auto">
-          <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 ">
-            {games.map((card, index) => (
-              <div key={index} className="book">
-                <div className="page">
-                  <div >
-                    <img
-                      src={card.image}
-                      className="front-page  bg-cover bg-center relative border border-blue-500"
-                      alt=""
-                    />
-                    <div className="after-content bg-center absolute inset-0 opacity-0 hover:opacity-100 transition duration-300">
+          <div className="flex justify-center items-center">
+            <div className="grid md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+              {games.map((card, index) => (
+                <div key={index} className="book">
+                  <div className="page">
+                    <div>
                       <img
                         src={card.image}
-                        alt="Content"
-                        className="h-full w-full"
+                        className="front-page bg-cover bg-center relative border border-blue-500"
+                        alt=""
                       />
+                      <div className="after-content bg-center absolute inset-0 opacity-0 hover:opacity-100 transition duration-300">
+                        <img
+                          src={card.image}
+                          alt="Content"
+                          className="h-full w-full"
+                        />
+                      </div>
+                    </div>
+                    <div className="back-page">
+                      <p>Surprise! SAFOAN BROOOOOOOOOOO</p>
+                      <Link to={`/games/${card._id}`}>
+                        <button className="btn">Details</button>
+                      </Link>
                     </div>
                   </div>
-                  <div className="back-page">
-                    <p>Surprise! SAFOAN BROOOOOOOOOOO</p>
-                    <Link to={`/games/${card._id}`}>
-                    <button className="btn">Details</button>
-                    </Link>
-                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
