016fa6e7b8fe1e4696b02a7a44f63f7c6f85952d
jakariachynabil10 <jakariachynabil@gmail.com>
2023-09-07 19:01:44 +0600

    price pool section is now responsive

--- src/Pages/Dashboard/Admin/Users/Users.jsx
+++ src/Pages/Dashboard/Admin/Users/Users.jsx
@@ -11,6 +11,7 @@ const Users = () => {
     axiosSecure.get(`/users`)
     .then(data=>{
       setUsers(data.data)
+      console.log(data)
     })
     // fetch(url)
     //   .then((res) => res.json())
@@ -58,8 +59,8 @@ const Users = () => {
       <h3 className="text-3xl font-semibold my-4">
         Total Users: {users.length}
       </h3>
-      <div className="border">
-        <table className="table  w-full">
+      <div className="overflow-x-scroll border lg:overflow-hidden">
+        <table className="table">
           {/* head */}
           <thead className="text-white bg-green-500 text-xl">
             <tr>
