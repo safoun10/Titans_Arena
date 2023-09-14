b66f5922987d0ab0d958460f320996d45e3a9cba
Safoun <safoun.official@gmail.com>
2023-09-14 18:26:44 +0600

    social link redux code added

--- src/Components/Shared/AnimatedTitle/Titile.jsx
+++ src/Components/Shared/AnimatedTitle/Titile.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-// import "./AnimatedTitle.css";
 
 const Title = ({ primaryText, secondaryText }) => {
   return (
     <div>
-      {/* <div id="wrapper" className="mx-auto text-center">
-        <div id="bar" className="w-1/2"></div>
-        <div id="bar2" className=""></div>
-        <h1>
-          <div className="main">
-            <h1 className="main-heading">
-              <span className="main-heading-primary text-center">{primaryText}</span>
-              <span className="main-heading-secondary">{secondaryText}</span>
-            </h1>
-          </div>
-        </h1>
-        <div id="bar2"></div>
-      </div> */}
        <div className="text-center my-10 lg:mx-0 mx-5">
         <h3 className="text-[#68fb9a] font-semibold text-lg uppercase">{primaryText}</h3>
         <h1 className="text-white text-4xl font-bold mb-2  uppercase">{secondaryText}</h1>
