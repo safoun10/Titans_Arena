1e75cdeb71b5a3f402564526807fc34ccea676c9
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-09-14 19:25:22 +0600

    solved

--- src/Pages/Dashboard/DashboardComponents/ProfileEdit.jsx
+++ src/Pages/Dashboard/DashboardComponents/ProfileEdit.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
+import Title from "../../../Components/Shared/AnimatedTitle/Titile";
 import useAuth from "../../../Hooks/useAuth";
 
 const ProfileEdit = () => {
@@ -67,155 +68,165 @@ const ProfileEdit = () => {
 
   console.log(userInfo?.bio);
   return (
-    <div className="pt-6 w-full  shadow-xl px-5 text-white">
-      <h2 className="text-center underline font-bold text-2xl lg:text-4xl italic pb-4">
-        Edit Profile Information
-      </h2>
+    <div className="pt-1 w-full bg-slate-800 shadow-xl px-5 text-white">
+      <Title primaryText="Update Your" secondaryText="profile Information" />
 
       <form onSubmit={handleProfileUpdate}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-          <div className="form-control">
-            <label className="label">
-              <span className="">Your Name</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              className="input input-bordered"
-              defaultValue={userInfo?.name || user?.displayName}
-              placeholder="name"
-            />
+        <div className="flex border">
+          <div className="w-1/2 ">
+            <div className="flex justify-center py-4">
+              <img
+                className="rounded-full w-36 border-4 p-2"
+                src={user?.photoURL}
+                alt=""
+              />
+            </div>
+            <h3 className="text-center text-3xl font-bold">{userInfo?.name}</h3>
+            
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="">Username</span>
-            </label>
-            <input
-              type="text"
-              name="username"
-              defaultValue={userInfo?.username}
-              className="input input-bordered"
-              placeholder="Username"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="">Phone Number</span>
-            </label>
-            <input
-              type="text"
-              name="phoneNumber"
-              className="input input-bordered"
-              defaultValue={userInfo?.phoneNumber}
-              placeholder="Phone Number"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="">Your Email</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="input input-bordered"
-              value={userInfo?.email}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="">Photo url</span>
-            </label>
-            <input
-              type="text"
-              name="photoURL"
-              className="input input-bordered"
-              defaultValue={userInfo?.photoURL || user?.photoURL}
-              placeholder="Photo url"
-            />
-          </div>
-        </div>
-        <div className="flex justify-between gap-2">
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Facebook</span>
-            </label>
-            <input
-              type="text"
-              name="facebook"
-              className="input input-bordered "
-              defaultValue={userInfo?.facebook}
-              placeholder="Facebook"
-            />
-          </div>
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Twitter</span>
-            </label>
-            <input
-              type="text"
-              name="twitter"
-              className="input input-bordered"
-              defaultValue={userInfo?.twitter}
-              placeholder="Twitter"
-            />
-          </div>
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Instagram</span>
-            </label>
-            <input
-              type="text"
-              name="instagram"
-              className="input input-bordered"
-              defaultValue={userInfo?.instagram}
-              placeholder="Instagram"
-            />
-          </div>
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Youtube</span>
-            </label>
-            <input
-              type="text"
-              name="youtube"
-              className="input input-bordered"
-              defaultValue={userInfo?.youtube}
-              placeholder="Youtube"
-            />
-          </div>
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Discord</span>
-            </label>
-            <input
-              type="text"
-              name="discord"
-              className="input input-bordered"
-              defaultValue={userInfo?.discord}
-              placeholder="Discord"
-            />
-          </div>
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="">Tiktok</span>
-            </label>
-            <input
-              type="text"
-              name="tiktok"
-              className="input input-bordered"
-              defaultValue={userInfo?.tiktok}
-              placeholder="Tiktok"
-            />
-          </div>
+          <div className="w-1/2 border p-4 "> <div className="form-control">
+          <label className="label">
+            <span className="text-xl font-bold ">Your Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.name || user?.displayName}
+            placeholder="name"
+          />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="">Add Bio</span>
+            <span className="text-xl font-bold">Username</span>
+          </label>
+          <input
+            type="text"
+            name="username"
+            defaultValue={userInfo?.username}
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-xl font-bold">Phone Number</span>
+          </label>
+          <input
+            type="text"
+            name="phoneNumber"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.phoneNumber}
+            placeholder="Phone Number"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-xl font-bold">Your Email</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            value={userInfo?.email}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-xl font-bold">Photo url</span>
+          </label>
+          <input
+            type="text"
+            name="photoURL"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.photoURL || user?.photoURL}
+            placeholder="Photo url"
+          />
+        </div></div>
+        </div>
+       
+
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Facebook</span>
+          </label>
+          <input
+            type="text"
+            name="facebook"
+            className="input input-bordered rounded-none "
+            defaultValue={userInfo?.facebook}
+            placeholder="Facebook"
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Twitter</span>
+          </label>
+          <input
+            type="text"
+            name="twitter"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.twitter}
+            placeholder="Twitter"
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Instagram</span>
+          </label>
+          <input
+            type="text"
+            name="instagram"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.instagram}
+            placeholder="Instagram"
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Youtube</span>
+          </label>
+          <input
+            type="text"
+            name="youtube"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.youtube}
+            placeholder="Youtube"
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Discord</span>
+          </label>
+          <input
+            type="text"
+            name="discord"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.discord}
+            placeholder="Discord"
+          />
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="text-xl font-bold">Tiktok</span>
+          </label>
+          <input
+            type="text"
+            name="tiktok"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
+            defaultValue={userInfo?.tiktok}
+            placeholder="Tiktok"
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="text-xl font-bold">Add Bio</span>
           </label>
           <textarea
             type="text"
             name="bio"
-            className="input input-bordered"
+            className="input input-bordered border-white text-lg font-semibold rounded-none bg-transparent"
             defaultValue={userInfo?.bio}
             placeholder="Add Your Bio"
           />
